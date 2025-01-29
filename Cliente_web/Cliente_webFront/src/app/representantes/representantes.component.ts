3cb88ae44580af5052dc4b2296d0cd92b49732cd
BorisMejias <128553727+BorisMejiasII@users.noreply.github.com>
2025-01-29 12:29:16 -0300

    Comenzando CRUD de Representantes
    +Fueron iniciados los CRUD de los representantes con el apartado "READ", es decir, ahora se pueden ver los usuarios cargados desde la BD

--- Cliente_web/Cliente_webFront/src/app/representantes/representantes.component.ts
+++ Cliente_web/Cliente_webFront/src/app/representantes/representantes.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {NgForOf} from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-representantes',
-  imports: [],
   templateUrl: './representantes.component.html',
-  standalone: true,
-  styleUrl: './representantes.component.css'
+  styleUrl: './representantes.component.css',
+  imports: [
+    NgForOf,
+    HttpClientModule
+  ],
+  standalone: true
 })
-export class RepresentantesComponent {
+export class RepresentantesComponent implements OnInit {
+  representantes: any[] = []; // Lista para almacenar los datos de la API
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.obtenerRepresentantes(); // Cargar datos al iniciar el componente
+  }
+
+  obtenerRepresentantes() {
+    this.http.get<any>('http://localhost:8000/api/representantes/pagina/1')
+      .subscribe(
+        data => {
+          this.representantes = data; // Asigna los datos obtenidos a la variable
+        },
+        error => {
+          console.error('Error al obtener representantes:', error);
+        }
+      );
+  }
 }
