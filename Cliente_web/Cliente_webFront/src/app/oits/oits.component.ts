25db506f0826e748efa44e29c14c343c1de07269
BorisMejias <128553727+BorisMejiasII@users.noreply.github.com>
2025-01-31 10:00:04 -0300

    CRUD de Itos
    +Se comenzaron los crud de los Itos, empezando por el "Read" el cual ya fue terminado

--- Cliente_web/Cliente_webFront/src/app/oits/oits.component.ts
+++ Cliente_web/Cliente_webFront/src/app/oits/oits.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {NgForOf, NgIf} from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-oits',
-  imports: [],
+  imports: [
+    NgForOf,
+    HttpClientModule,
+    FormsModule,
+  ],
   templateUrl: './oits.component.html',
   standalone: true,
   styleUrl: './oits.component.css'
 })
-export class OITsComponent {
+export class OITsComponent implements OnInit{
+  itos: any[] = [];
+
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit(): void {
+      this.ObtenerItos();
+  }
+
+  ObtenerItos(){
+    this.http.get<any>('http://localhost:8000/api/itos/pagina/1')
+      .subscribe(
+        data => {
+          this.itos = data; // Asigna los datos obtenidos a la variable
+        },
+        error => {
+          console.error('Error al obtener representantes:', error);
+        }
+      );
+
+
+  }
+
+
+
+
 
 }
